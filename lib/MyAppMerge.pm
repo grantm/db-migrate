47e5cc1c0bdc9a7df20b9ccaa373e37f43b5fbfe
Grant McLean <grant@mclean.net.nz>
2016-04-13 19:18:21 +1200

    remove cruft

--- lib/MyAppMerge.pm
+++ lib/MyAppMerge.pm
@@ -21,26 +21,4 @@ my $table_handlers = [
 #    return $table_handlers;
 #}
 
-
-sub pre_map_request {
-    my($self, $rec) = @_;
-
-    return 'SKIP' if $rec->{request_id} <= 0;
-}
-
-
-sub map_request_id {
-    my($self, $request_id) = @_;
-
-    return 100_000 + $request_id;
-}
-
-
-sub post_map_saved_queries {
-    my($self, $rec) = @_;
-
-    $rec->{query_name} = 'ES ' . $rec->{query_name};
-}
-
-
 __PACKAGE__->meta->make_immutable;
